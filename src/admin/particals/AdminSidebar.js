e7f8729924cc2e613fc596e4018fe98a86763466
SOHILVAGHELA <sohilvaghela7863@gmail.com>
2024-11-04 20:12:21 +0530

    add logout messageon admin side

--- src/admin/particals/AdminSidebar.js
+++ src/admin/particals/AdminSidebar.js
@@ -126,6 +126,7 @@ import React, { useEffect, useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
 import { jwtDecode } from "jwt-decode";
+import toast from "react-hot-toast";
 
 const AdminSidebar = () => {
   const [auth, setAuth] = useAuth();
@@ -153,6 +154,7 @@ const AdminSidebar = () => {
     localStorage.clear("auth");
     localStorage.clear("token");
     setAuth({ user: null, token: "" });
+    toast.success("Logout successfully");
     navigate("/login");
   };
 
@@ -166,7 +168,12 @@ const AdminSidebar = () => {
       <button
         className="btn btn-primary d-md-none"
         onClick={toggleSidebar}
-        style={{ position: "absolute", top: "10px", left: "10px", zIndex: 1000 }}
+        style={{
+          position: "absolute",
+          top: "10px",
+          left: "10px",
+          zIndex: 1000,
+        }}
       >
         <i className="fas fa-bars"></i>
       </button>
@@ -192,7 +199,9 @@ const AdminSidebar = () => {
           {/* Sidebar user panel */}
           <div className="user-panel mt-3 pb-3 mb-3 d-flex">
             <div className="info">
-              <a href="#" className="d-block">Admin</a>
+              <a href="#" className="d-block">
+                Admin
+              </a>
             </div>
           </div>
 
@@ -210,37 +219,67 @@ const AdminSidebar = () => {
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink to="proposal" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
+                <NavLink
+                  to="proposal"
+                  className={({ isActive }) =>
+                    isActive ? "nav-link active" : "nav-link"
+                  }
+                >
                   <i className="nav-icon fas fa-file-signature"></i>
                   <p>Proposals</p>
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink to="proposaltempletes" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
+                <NavLink
+                  to="proposaltempletes"
+                  className={({ isActive }) =>
+                    isActive ? "nav-link active" : "nav-link"
+                  }
+                >
                   <i className="nav-icon fas fa-file-alt"></i>
                   <p>Proposal Templates</p>
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink to="allusers" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
+                <NavLink
+                  to="allusers"
+                  className={({ isActive }) =>
+                    isActive ? "nav-link active" : "nav-link"
+                  }
+                >
                   <i className="nav-icon fas fa-users"></i>
                   <p>Clients</p>
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink to="categories" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
+                <NavLink
+                  to="categories"
+                  className={({ isActive }) =>
+                    isActive ? "nav-link active" : "nav-link"
+                  }
+                >
                   <i className="nav-icon fas fa-th-list"></i>
                   <p>Categories</p>
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink to="products" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
+                <NavLink
+                  to="products"
+                  className={({ isActive }) =>
+                    isActive ? "nav-link active" : "nav-link"
+                  }
+                >
                   <i className="nav-icon fas fa-tags"></i>
                   <p>Products</p>
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink to="orders" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
+                <NavLink
+                  to="orders"
+                  className={({ isActive }) =>
+                    isActive ? "nav-link active" : "nav-link"
+                  }
+                >
                   <i className="nav-icon fas fa-shopping-cart"></i>
                   <p>Orders</p>
                 </NavLink>
@@ -261,8 +300,14 @@ const AdminSidebar = () => {
                 className="bi bi-box-arrow-right"
                 viewBox="0 0 16 16"
               >
-                <path fillRule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"/>
-                <path fillRule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"/>
+                <path
+                  fillRule="evenodd"
+                  d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"
+                />
+                <path
+                  fillRule="evenodd"
+                  d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"
+                />
               </svg>
               Logout
             </button>
